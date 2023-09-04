769346469e389cbc11cc933d3c6309eb0da1581f
surya <111235697+surya2519@users.noreply.github.com>
2023-09-04 19:13:10 +0530

    add js code

---
+++ script.js
@@ -0,0 +1,8 @@
+script.js
+
+// JavaScript to handle button click event
+document.getElementById("myButton").addEventListener("click", function() {
+    // Change the text when the button is clicked
+    document.getElementById("output").textContent = "Button Clicked!";
+});
+  
